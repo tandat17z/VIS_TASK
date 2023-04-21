a0608c88d49d7ec08ee580baaa8cf23bb67f1a4c
Le Tan Dat <126872123+tandat17z@users.noreply.github.com>
2023-04-22 00:13:59 +0700

    Create agentWTTD_v2.py

---
+++ task thang 4/agentWTTD_v2.py
@@ -0,0 +1,34 @@
+def DataAgent():
+  per = []
+  per.append(np.zeros(1))
+  return per
+
+def Test(state, per):
+  validActions = getValidActions(state)
+  actions = np.where(validActions)[0]
+
+  if state[13] > 7:
+    if 0 in actions:
+      return 0, per
+
+  if 11 in actions:
+      return 11, per
+
+  if state[53]:
+    if np.sum(state[76: 83]) and 1 in actions:
+      return 1, per
+
+    for i in np.array([3, 4, 6, 7, 5, 2]):
+      if i in actions:
+        return i, per
+        
+  if state[61]:
+    if np.sum(state[74:81]) and 1 in actions:
+      return 1, per
+
+    for i in np.array([6, 2, 3, 5, 7, 4]):
+      if i in actions:
+        return i, per
+      
+  act = np.random.choice(actions)
+  return act, per
